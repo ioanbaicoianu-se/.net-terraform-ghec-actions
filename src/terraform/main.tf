e97143d67107cd42ff321d2208d2bd3e3cb0a69d
Ioan <ioan.baicoianu@se.com>
2024-09-24 11:11:37 +0200

    deploying a web app using terraform and github actions

---
+++ src/terraform/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.application_name}-${var.environment_name}"
+  location = var.location
+}
+
+resource "random_string" "name" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+data "azurerm_client_config" "current" {}
+
