26bbe1ecbcc4ae0c11d8022030ae386b1038eeef
Ioan <ioan.baicoianu@se.com>
2024-09-30 15:43:42 +0200

    updated fn.tf

--- src/terraform/keyvault.tf
+++ src/terraform/keyvault.tf
@@ -6,20 +6,27 @@ resource "azurerm_key_vault" "main" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
-  sku_name                    = "standard"
+
+  sku_name = "standard"
 }
+
 resource "azurerm_key_vault_access_policy" "terraform-user" {
+
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
+
   secret_permissions = [
     "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
   ]
 }
-resource "azurerm_key_vault_access_policy" "functions-user" {
+
+resource "azurerm_key_vault_access_policy" "function-user" {
+
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_user_assigned_identity.functions.principal_id
+
   secret_permissions = [
     "Get"
   ]
